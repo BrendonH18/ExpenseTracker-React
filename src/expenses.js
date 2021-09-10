69788eda007b1d317e9fa2304e75f4f322c4ce92
SunShark3 <hall.brendonp@gmail.com>
2021-09-09 23:39:49 -0400

    initial commit

---
+++ src/expenses.js
@@ -0,0 +1,12 @@
+const data = [
+  {
+    id: 0,
+    date: '01/02/2021',
+    description: 'Birthday Gift',
+    where: 'Ebay',
+    amount: '$33.00',
+    category: 'Housing'
+  }
+]
+
+export default data
